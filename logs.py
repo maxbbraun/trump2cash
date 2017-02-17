1864ccba7abdedc39470d5a6f56c8346079c54d4
Max Braun <max.braun@gmail.com>
2017-02-17 19:31:45 +0000

    Fail over to local logs after cloud log retries.

--- logs.py
+++ logs.py
@@ -25,68 +25,92 @@ class Logs:
 
     def __init__(self, name, to_cloud=True):
         self.to_cloud = to_cloud
+
+        # Initialize the local file logger.
+        self.local_logger = getLogger(name)
+        basicConfig(format=LOGS_FORMAT, level=NOTSET, filename=LOG_FILE)
+
+        # If requested, also initialize the Stackdriver logging and error
+        # reporting clients.
         if self.to_cloud:
-            # Use the Stackdriver logging and error reporting clients.
-            self.logger = logging.Client().logger(name)
+            self.cloud_logger = logging.Client().logger(name)
             self.error_client = error_reporting.Client()
-        else:
-            # Log to a local file.
-            self.logger = getLogger(name)
-            basicConfig(format=LOGS_FORMAT, level=NOTSET, filename=LOG_FILE)
 
     def debug(self, text):
         """Logs at the DEBUG level."""
 
         if self.to_cloud:
-            self.safe_log_text(text, severity="DEBUG")
+            self.safe_cloud_log_text(text, severity="DEBUG")
         else:
-            self.logger.debug(text)
+            self.local_logger.debug(text)
 
     def info(self, text):
         """Logs at the INFO level."""
 
         if self.to_cloud:
-            self.safe_log_text(text, severity="INFO")
+            self.safe_cloud_log_text(text, severity="INFO")
         else:
-            self.logger.info(text)
+            self.local_logger.info(text)
 
     def warn(self, text):
         """Logs at the WARNING level."""
 
         if self.to_cloud:
-            self.safe_log_text(text, severity="WARNING")
+            self.safe_cloud_log_text(text, severity="WARNING")
         else:
-            self.logger.warning(text)
+            self.local_logger.warning(text)
 
     def error(self, text):
         """Logs at the ERROR level."""
 
         if self.to_cloud:
-            self.safe_log_text(text, severity="ERROR")
+            self.safe_cloud_log_text(text, severity="ERROR")
         else:
-            self.logger.error(text)
+            self.local_logger.error(text)
 
     def catch(self, exception):
         """Logs an exception."""
 
         if self.to_cloud:
             self.safe_report_exception()
-            self.safe_log_text(str(exception), severity="CRITICAL")
+            self.safe_cloud_log_text(str(exception), severity="CRITICAL")
         else:
-            self.logger.critical(str(exception))
+            self.local_logger.critical(str(exception))
 
-    @on_exception(expo, BaseException, max_tries=5)
-    def safe_log_text(self, text, severity):
-        """Logs to the cloud and retries up to 5 times with exponential backoff
+    def safe_cloud_log_text(self, text, severity):
+        """Logs to the cloud, retries if necessary, and eventually fails over
+        to local logs.
+        """
+
+        try:
+            self.retry_cloud_log_text(text, severity)
+        except BaseException as exception:
+            self.local_logger.error("Failed to log to cloud: %s %s %s" %
+                                    (exception, severity, text))
+
+    @on_exception(expo, BaseException, max_tries=7)
+    def retry_cloud_log_text(self, text, severity):
+        """Logs to the cloud and retries up to 7 times with exponential backoff
         if the upload fails.
         """
 
-        self.logger.log_text(text, severity=severity)
+        self.cloud_logger.log_text(text, severity=severity)
 
-    @on_exception(expo, BaseException, max_tries=5)
     def safe_report_exception(self):
-        """Reports the latest exception to the cloud and retries up to 5 times
-        with exponential backoff if the upload fails.
+        """Reports the latest exception, retries if necessary, and eventually
+        fails over to local logs.
+        """
+
+        try:
+            self.retry_report_exception()
+        except BaseException as exception:
+            self.local_logger.error("Failed to report exception: %s" %
+                                    exception)
+
+    @on_exception(expo, BaseException, max_tries=7)
+    def retry_report_exception(self):
+        """Reports the latest exception and retries up to 7 times with
+        exponential backoff if the upload fails.
         """
 
         self.error_client.report_exception()
